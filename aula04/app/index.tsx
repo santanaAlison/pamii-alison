ce3844fefe81bb30d3b808de8d346f46da2a83ca
alison santana <alison.santana2@etec.sp.gov.br>
2025-03-21 22:25:44 -0300

    aula de banco de dados firebase do dia 21/03

--- aula04/app/index.tsx
+++ aula04/app/index.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
-export default function Index() {
+const firebaseConfig = {
+  apiKey: "AIzaSyDxjpHUZzYpjR5gaIIGPf1yXiO5pYgbQB8",
+  authDomain: "meu-primeiro-firebase-b1cb0.firebaseapp.com",
+  projectId: "meu-primeiro-firebase-b1cb0",
+  storageBucket: "meu-primeiro-firebase-b1cb0.firebasestorage.app",
+  messagingSenderId: "46059561627",
+  appId: "1:46059561627:web:8c9aab669709832ebd1c3e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+import React, {useEffect, useState } from "react";
+import { View, Text, FlatList } from 'react-native';
+
+export default function App() {
+  const [ nomes, setNomes] = useState([]);
+  
+  useEffect (() => {
+    const fetchData = async () => {
+      const nomesCollection = firebase.firestore().collection('Nomes');
+      const snapshot = await nomesCollection.get();
+
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      
+      });
+
+      setNomes(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <View style={{ flex: 1, justifyContent: 'center',
+      alignItems: 'center' }}>
+        <Text> Lista de Nomes </Text>  
+        <FlatList
+        data={nomes}
+        keyExtractor={(item) => item,id}
+        renderItem={({ item }) => (
+          <View> 
+            <Text>{item.Nome} {item.Sobrenome}</Text>
+            </View>
+        )
+      </View>
+  )
 }
